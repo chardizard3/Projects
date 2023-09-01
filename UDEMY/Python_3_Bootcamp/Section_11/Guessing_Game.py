206405e586d379cbc2a9a09936c37c5c7b63777c
chardizard3 <richardpalabasan@gmail.com>
2023-09-01 14:45:12 +0800

    Create Guessing_Game.py

---
+++ UDEMY/Python_3_Bootcamp/Section_11/Guessing_Game.py
@@ -0,0 +1,26 @@
+from random import randint
+
+num = randint(1,10)
+print (num)
+
+guess = 0
+
+while guess != num :
+    guess = input ("Guess a number between 1 and 10: ")
+    guess = int(guess)
+
+    if guess > num :
+        print ("Too high, try again!")
+        
+    elif guess < num :
+        print ("Too low, try again!")
+       
+    else :
+        print ("You guessed it! You won!")
+        again = input("Do you want to keep playing? (y/n) ")
+        if again == "y" :
+            num = randint(1,10)
+            #guess = input ("Guess a number between 1 and 10: ")
+
+if guess == num:
+    print ("You guessed it! You won!")
