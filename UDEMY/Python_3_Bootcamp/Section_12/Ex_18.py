6da3152583ad1a9d1ae80effa9ef91aaf4e52b7d
chardizard3 <richardpalabasan@gmail.com>
2023-09-07 10:14:54 +0800

    Exercises

---
+++ UDEMY/Python_3_Bootcamp/Section_12/Ex_18.py
@@ -0,0 +1,20 @@
+"""
+Lists Basics Exercise
+Now that we've learned about lists, let's get some practice. 
+See the problem statements in the comments below:
+"""
+
+# Initially create an empty list called instructors
+
+# Add the following strings to the instructors list 
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+
+# Run the tests to make sure you've done this correctly!
+
+instructors = []
+instructors.append("Colt")
+instructors.append("Blue")
+instructors.append("Lisa")
+print(instructors)
